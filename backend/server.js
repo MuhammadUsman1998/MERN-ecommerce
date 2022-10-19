96ef203dd2975cf412a22d7f8a2233eaf7641be9
MuhammadUsman1998 <muhammad.mianusman15@gmail.com>
2022-10-19 14:10:52 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,35 @@
-const express = require("express")
-const products = require("./data/products")
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+// import products from "./data/products.js"
+import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
+import colors from "colors"
+import productRoute from "./routes/productRoute.js"
+dotenv.config()
+connectDB()
 const app = express()
 
 app.get('/', (req, res)=>{
+console.log("data send by ===>",req.query.name);
 res.send("Api is Running.....")
 })
-app.get('/api/products/' , (req,res)=>{
-    res.json(products)
-})
 
-app.get('/api/products/:id' ,(req,res)=>{
-    const product= products?.find((p)=>{ 
-        return  p._id === req.params.id})
-    res.json(product)
-})
+app.use("/api/products",productRoute)
 
-app.listen(5000,console.log("Server running on 5000 port"))
+app.use(notFound)
+
+app.use(errorHandler)
+
+// app.get('/api/products/' , (req,res)=>{
+//     res.json(products)
+// })
+
+// app.get('/api/products/:id' ,(req,res)=>{
+//     const product= products?.find((p)=>{ 
+//         return  p._id === req.params.id})
+//     res.json(product)
+// })
+
+const PORT =process.env.PORT || 5000
+
+app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow))
